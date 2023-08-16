9e0fc6bb567d06d1525dbe475e8c034e2804dac6
Ku4eryavyy <101946992+Ku4eryavyy@users.noreply.github.com>
2023-08-16 17:38:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const nameOfUser = prompt('Whats your name?');
 
 if(nameOfUser){
-
+    
     alert("Hello, " + nameOfUser + "!" + " How are you?");
 }
 
 else {
+    
     alert("You didn't enter your name :(");
 }
 
