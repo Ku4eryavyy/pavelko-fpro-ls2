f743fe018682932ed44414a6ec2e15b67ce7693f
Kucheryavyy <donatellopavelko@gmail.com>
2023-08-16 17:34:49 +0200

    created a program

---
+++ script.js
@@ -0,0 +1,14 @@
+const nameOfUser = prompt('Whats your name?');
+
+if(nameOfUser){
+
+    alert("Hello, " + nameOfUser + "!" + " How are you?");
+}
+
+else {
+    alert("You didn't enter your name :(")
+}
+
+console.log(nameOfUser);
+
+
